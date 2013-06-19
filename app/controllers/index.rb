f43dc5d3e2582170b1c190f7b9a82c09cd189630
Uzma Rahman <uzma.rahman@gmail.com>
2013-06-18 19:30:31 -0700

    removed password from files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,12 @@
 get '/' do
 
- client = Octokit::Client.new(:login => "UzmaKR", :password => "Temp2013!")
+ client = Octokit::Client.new(:login => "UzmaKR", :password => ENV['GITHUB_PASSWORD'])
  @api_user_info = client.user "UzmaKR"
  @api_follow = client.follow "Stephenitis"
  @api_commits = client.commits "mattbarackman/recent_tweets_1"
  @api_list_commits = client.list_commits "mattbarackman/recent_tweets_1"
 
- @api_repo_contents= client::Contents.contents("UzmaKR/anagram_server")
+ @api_repo_contents= Octokit.contents("UzmaKR/anagram_server")
   # Look in app/views/index.erb
  erb :index
 end
